4d7f94f8b2bf5c3c7b79502644daee7193eaae97
Alex Rowan <arowanuk@gmail.com>
2020-05-24 23:17:39 +0100

    css for host

--- src/pages/Host/index.js
+++ src/pages/Host/index.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect, useRef } from "react";
+import words from "./numbers";
+import "./index.css";
 
 function Host() {
   const [gameId, setGameId] = useState(null);
@@ -9,6 +11,7 @@ function Host() {
   const [used, setUsed] = useState([]);
   const timer = useRef(null);
   const [error, setError] = useState(null);
+  const [historyVisible, setHistoryVisible] = useState(false);
 
   useEffect(() => {
     setGameId(Math.floor(Math.random() * 10000 + 1));
@@ -16,7 +19,7 @@ function Host() {
 
   const pickNumber = () => {
     setIsLoaded(false);
-    fetch(`http://localhost:3000/pick/${gameId}`)
+    fetch(`${process.env.REACT_APP_API_URL}/pick/${gameId}`)
       .then((res) => res.json())
       .then(
         (result) => {
@@ -53,28 +56,63 @@ function Host() {
 
   if (error) {
     return <div>Error: {error.message}</div>;
-  } else if (!isLoaded) {
-    return <div>Loading...</div>;
   } else {
     return (
       <div className="host">
-        <h2>Game ID: {gameId}</h2>
-        <input
-          type="range"
-          min="1"
-          max="20"
-          value={delay / 1000}
-          onChange={handleChange}
-          disabled={running}
-        ></input>
-        {delay / 1000} second delay
-        <div>
-          <h3 className="host__last-number">{lastNumber}</h3>
+        <h2 className={`host__id ${running ? "host__id--grayed" : ""}`}>
+          Game ID: {gameId}
+        </h2>
 
-          {!running && <button onClick={() => setRunning(true)}>Start!</button>}
-          {running && <button onClick={() => setRunning(false)}>Stop</button>}
+        <div className={`host__draw ${!running ? "host__draw--grayed" : ""}`}>
+          <h3
+            className={`host__last-number ${
+              !running ? "host__last-number--grayed" : ""
+            }`}
+          >
+            {lastNumber}
+          </h3>
+          <span className="host__words">"{words[lastNumber]}"</span>
         </div>
-        <div className="host__history">
+
+        <div className="host__controller">
+          {!running && (
+            <button
+              className="host__button host__button--start"
+              onClick={() => setRunning(true)}
+            >
+              Start
+            </button>
+          )}
+          {running && (
+            <button
+              className="host__button host__button--stop"
+              onClick={() => setRunning(false)}
+            >
+              Stop
+            </button>
+          )}
+          <button
+            className="host__button host__button--history"
+            onClick={() => setHistoryVisible(!historyVisible)}
+          >
+            Toggle history
+          </button>
+          <label>{delay / 1000} second delay</label>
+          <input
+            type="range"
+            min="1"
+            max="20"
+            value={delay / 1000}
+            onChange={handleChange}
+            disabled={running}
+          ></input>
+        </div>
+
+        <div
+          className={`host__history ${
+            historyVisible ? "host__history--visible" : ""
+          }`}
+        >
           {used.map((item, idx) => (
             <div key={`history-${idx}`}>{item}</div>
           ))}
